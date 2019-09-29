79afb1200b76ef7cc1c2a7bcdda96c024b953ab6
Lincoln Green <lincoln@jango>
2019-09-29 10:50:35 -0700

    Initial commit

---
+++ manifest.js
@@ -0,0 +1,16 @@
+images = [
+	{ 
+		"img_path": "./image.jpg", 
+		"meta": {
+			"lang": "en",
+			"device": "iPhone SE"
+		}
+	},
+	{ 
+		"img_path": "./image.jpg", 
+		"meta": {
+			"lang": "en",
+			"device": "iPhone SE"
+		}
+	}
+]
